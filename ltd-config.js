a27e38dd6c04f075d8c0ed88c37d2ad2e8cc6328
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-21 16:21:10 +0000

    Deploying to pages from @ localizethedocs/urllib3-docs-l10n@01b963f2f94079f833e39e78ea00bcfcd2be8ee7 🚀

---
+++ ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",    "Development"],
+    ["2",       "Release 2.x"],
+    ["1",       "Release 1.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/urllib3-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/urllib3-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/urllib3-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/urllib3-docs-l10n"],
+  ]
+};
